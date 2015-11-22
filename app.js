6452b97249905c2d604ed3d6beb1ca851dec3a0a
olegls2000 <pasechnik.o.n@gmail.com>
2015-11-22 13:06:40 +0200

    REN-25[FrontEnd] Создание формы регистрации (взаимодействующей по REST): создание сервера.

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 /**
  * Created by opasichnyk on 11/21/2015.
  */
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var app = express();
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+http.createServer(app).listen(3000, function() {
+    console.log('Express server listening on port' + 3000);
+});
