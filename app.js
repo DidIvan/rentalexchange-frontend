544e7b5979f2c1535259135a8a93b6f6bba45560
vitaliypryhodko <vitaliypryhodko@gmail.com>
2015-11-23 22:36:40 +0300

    adding config and logger

--- app.js
+++ app.js
@@ -4,10 +4,14 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
+var config = require('config');
+var log = require('libs/log')(module);
 var app = express();
+
+app.set('port', config.get('port'));
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
-http.createServer(app).listen(3000, function() {
-    console.log('Express server listening on port' + 3000);
+http.createServer(app).listen(config.get('port'), function() {
+    log.info('Express server listening on port ' + config.get('port'));
 });
