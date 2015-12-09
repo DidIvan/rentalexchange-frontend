4161ce2ff376af9e468bc9f4b19f858dbfe2ab32
opasichnyk <pasechnik.o.n@gmail.com>
2015-12-09 21:24:38 +0200

    add validation to the component

--- public/js/components/registration.js
+++ public/js/components/registration.js
@@ -3,46 +3,42 @@
 var RegistrationForm = React.createClass({
 
     getInitialState: function () {
-    return {
-        email: '',
-        errMessage: '',
-        validIcon: ''
-    };
+        return {
+            email: '',
+            errMessage: '',
+            validIcon: ''
+        };
     }
     , handleEmailChange: function (e) {
         //validation
         this.setState({email: e.target.value});
-    this.setState({errMessage: ''});
-    if(validateEmail(this.state.email))
-    {
-        document.getElementById('icon_email_reg').style.color = 'green';
-        this.setState({validIcon: 'done'});
-    } else {
-        document.getElementById('icon_email_reg').style.color = 'red';
-        this.setState({validIcon: 'close'});
+        this.setState({errMessage: ''});
+        if  (this.validateEmail(this.state.email)) {
+            document.getElementById('icon_email_reg').style.color = 'green';
+            this.setState({validIcon: 'done'});
+        } else {
+            document.getElementById('icon_email_reg').style.color = 'red';
+            this.setState({validIcon: 'close'});
+        }
+
+
     }
 
-    function validateEmail(email) {
-        var re = /.+@.+\..+/i;
-        return re.test(email);
-    }
-    }
+    , validateEmail: function (email) {
+    var re = /.+@.+\..+/i;
+    return re.test(email);
+}
     , handleSubmit: function (e) {
         e.preventDefault();
         var email = this.state.email.trim();
 
-    if(!validateEmail(email))
-    {
-        this.setState({errMessage: 'Некорректный email!'});
-        return;
-    }
+        if (!this.validateEmail(email)) {
+            this.setState({errMessage: 'Некорректный email!'});
+            return;
+        }
 
-    function validateEmail(email) {
-        var re = /.+@.+\..+/i;
-        return re.test(email);
-    }
 
-    this.setState({errMessage: ''});
+        this.setState({errMessage: ''});
 
         $.ajax({
             url: this.props.url,
@@ -53,7 +49,7 @@ var RegistrationForm = React.createClass({
                 alert("SUCCESS POST");
             }.bind(this),
             error: function (xhr, status, err) {
-                alert("ERROR POST "+status);
+                alert("ERROR POST " + status);
             }.bind(this)
         });
         this.setState({email: ''});
@@ -62,23 +58,25 @@ var RegistrationForm = React.createClass({
     , render: function () {
         return (
             <form className="registrationForm" onSubmit={this.handleSubmit}>
-              <div className="row">
-                   <div className="col l9 s12">
-                       <input type="text" placeholder="Email"
-                       value={this.state.email}
-                       onChange={this.handleEmailChange}/>
-                   </div>
-                   <div className="col l3 s12"><i id="icon_email_reg" className="medium material-icons">{this.state.validIcon}</i></div>
-                   <div className="col l9 s12">
-                      <p><img src="../img/captcha.jpg" alt="captcha"></img></p>
-                   </div>
-                   <div className="col l9 s12">
-                      <button className="btn waves-effect light-blue lighten-1" type="submit" value="Registration">Зарегистрировать
-                      <i className="material-icons right">send</i>
-                      </button>
+                <div className="row">
+                    <div className="col l9 s12">
+                        <input type="text" placeholder="Email"
+                               value={this.state.email}
+                               onChange={this.handleEmailChange}/>
+                    </div>
+                    <div className="col l3 s12"><i id="icon_email_reg"
+                                                   className="medium material-icons">{this.state.validIcon}</i></div>
+                    <div className="col l9 s12">
+                        <p><img src="../img/captcha.jpg" alt="captcha"></img></p>
+                    </div>
+                    <div className="col l9 s12">
+                        <button className="btn waves-effect light-blue lighten-1" type="submit" value="Registration">
+                            Зарегистрировать
+                            <i className="material-icons right">send</i>
+                        </button>
                     </div>
                     <div className="col l12 s12"><p className="red-text">{this.state.errMessage}</p></div>
-               </div>
+                </div>
             </form>
         );
     }
