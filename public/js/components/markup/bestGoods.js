7f013dba9b5e0fdde1291d81a4f2358607945ade
opasichnyk <pasechnik.o.n@gmail.com>
2015-12-14 21:43:39 +0200

    refactoring

--- public/js/components/markup/bestGoods.js
+++ public/js/components/markup/bestGoods.js
@@ -4,84 +4,33 @@
 /** @jsx React.DOM */
 
 var BestGoods = React.createClass({
+    getInitialState: function () {
+        //TODO extract data from server
+    },
     render: function () {
         return (
             <div className="row">
-                <Goods1/>
-                <Goods2/>
-                <Goods3/>
-                <Goods4/>
+                <Goods image="img/Product.png" price="150" goodName="Name1"/>
+                <Goods image="img/Product.png" price="250" goodName="Name2"/>
+                <Goods image="img/Product.png" price="350" goodName="Name3"/>
+                <Goods image="img/Product.png" price="450" goodName="Name4"/>
             </div>
 
         )
     }
 });
 
-var Goods1 = React.createClass({
+var Goods = React.createClass({
     render: function () {
         return (
             <div className="col s12 m3 l3">
                 <div className="card z-depth-2">
                     <div className="card-image">
-                        <a href="#"><img src="img/Product.png"/></a>
+                        <a href="#"><img src={this.props.image}/></a>
                     </div>
                     <div className="card-action">
-                        <a href="#"><span className="price">150</span><span
-                            className="perweek">грн. за неделю</span></a>
-                    </div>
-                </div>
-            </div>
-        )
-    }
-});
-
-var Goods2 = React.createClass({
-    render: function () {
-        return (
-            <div className="col s12 m3 l3">
-                <div className="card z-depth-2">
-                    <div className="card-image">
-                        <a href="#"> <img src="img/Product.png"/></a>
-                    </div>
-                    <div className="card-action">
-                        <a href="#"><span className="price">150</span><span
-                            className="perweek">грн. за неделю</span></a>
-                    </div>
-                </div>
-            </div>
-        )
-    }
-});
-
-var Goods3 = React.createClass({
-    render: function () {
-        return (
-            <div className="col s12 m3 l3">
-                <div className="card z-depth-2">
-                    <div className="card-image">
-                        <a href="#"> <img src="img/Product.png"/></a>
-                    </div>
-                    <div className="card-action">
-                        <a href="#"><span className="price">150</span><span
-                            className="perweek">грн. за неделю</span></a>
-                    </div>
-                </div>
-            </div>
-        )
-    }
-});
-
-var Goods4 = React.createClass({
-    render: function () {
-        return (
-            <div className="col s12 m3 l3">
-                <div className="card z-depth-2">
-                    <div className="card-image">
-                        <a href="#"> <img src="img/Product.png"/></a>
-                    </div>
-                    <div className="card-action">
-                        <a href="#"><span className="price">150</span><span
-                            className="perweek">грн. за неделю</span></a>
+                        <a href="#"><span className="price">{this.props.price}</span><span
+                            className="perweek">{this.props.goodName}</span></a>
                     </div>
                 </div>
             </div>
