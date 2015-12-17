0af98ff0acd9eee4ebfe98b2d7060a0836290164
Ivan <Holy.vinil@gmail.com>
2015-12-17 23:26:17 +0200

    added modal window

---
+++ public/js/components/markup/captcha.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Ivan on 17.12.2015.
+ */
+/** @jsx React.DOM */
+
+//var Recaptcha = require('react-recaptcha');
+//
+//// specifying your onload callback function
+//var callback = function () {
+//    console.log('Done!!!!');
+//};
+//
+//ReactDOM.render(
+//    <Recaptcha
+//        sitekey="xxxxxxxxxxxxxxxxxxxx"
+//        render="explicit"
+//        onloadCallback={callback}
+//        />,
+//    document.getElementById('example')
+//);
