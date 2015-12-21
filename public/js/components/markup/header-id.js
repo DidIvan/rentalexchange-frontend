585265e72eb04a0893a5b1ed28c602a689445d68
Ivan <Holy.vinil@gmail.com>
2015-12-21 20:51:45 +0200

    some change in react

--- public/js/components/markup/header-id.js
+++ public/js/components/markup/header-id.js
@@ -34,22 +34,21 @@ var Auth = React.createClass({
     getInitialState: function () {
         return {isModalNeed: false};
     }
-    , registryClick: function(event){
+    , registryClick: function (event) {
         this.setState({isModalNeed: !this.state.isModalNeed});
     }
     ,
     render: function () {
         var modalComp = <div></div>;
         if (this.state.isModalNeed) {
-            modalComp = <ModalRedistrationWindow/>;
+            modalComp = <ModalRegistrationWindow/>;
         } else {
             modalComp = <div></div>;
         }
         return (
             <div>
                 <div className="entrance-menu right hide-on-med-and-down valign-wrapper">
-                    <button className="btn valign" id="login_button" onClick={this.registryClick}><span className="label">Зарегистрироваться</span>
-                    </button>
+                    <a className="waves-effect waves-light btn" href="#" onClick={this.registryClick}>Регистрация</a>
                 </div>
                 {modalComp}
             </div>
@@ -57,18 +56,46 @@ var Auth = React.createClass({
     }
 });
 
-var ModalRedistrationWindow = React.createClass({
+var ModalRegistrationWindow = React.createClass({
     render: function () {
         return (
-            <div id="modal-id" className="card-panel">
-                <form className="login-form">
-                    <div className="row center">
-                        <h5>Регистрация учётной записи пользователя</h5>
-                    </div>
-                </form>
+            <div className="modal">
+                <div className="card-panel">
+                    <form className="login-form">
+
+                        <div className="row center">
+                            <h5>Регистрация учётной записи пользователя</h5>
+                        </div>
+
+                        <div className="row margin">
+                            <div className="input-field col s12">
+                                <i className="mdi-communication-email prefix"></i>
+                                <input id="email" type="email"/>
+                                <label for="email" className="center-align">Email</label>
+                            </div>
+                        </div>
+
+                        <div className="row margin">
+                            <p className="center">На этот e-mail мы отправим письмо для проверки корректности указанного
+                                адреса. В
+                                письме будут
+                                содержаться инструкции по активации учётной записи пользователя с таким e-mail.</p>
+                        </div>
+                        <div className="row margin">
+                            <img src="img/captcha.jpg" alt="captcha"/>
+                        </div>
+
+                        <div className="row">
+                            <div className="input-field col s12">
+                                <a href="#" className="btn waves-effect waves-light col s12">Зарегистрироваться</a>
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
         )
     }
 });
+
 
 React.render(<Header/>, document.getElementById('header-id'));
