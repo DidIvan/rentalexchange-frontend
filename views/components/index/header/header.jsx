fc6a997c18332ee7d2815a0ff86f3c882ce58820
Ivan <Holy.vinil@gmail.com>
2016-02-27 19:20:58 +0200

    added component userinfo

--- views/components/index/header/header.jsx
+++ views/components/index/header/header.jsx
@@ -1,15 +1,24 @@
 var React = require("react"),
     Search = require("./search.jsx"),
-    Auth = require("./auth.jsx");
+    Auth = require("./auth.jsx"),
+    authenticator = require('app/authenticator'),
+    UserInfo = require('./userinfo.jsx');
 var Header = React.createClass({
     render: function () {
+        var chooseComponent;
+        if (authenticator.isAuthenticate) {
+            chooseComponent = <UserInfo/>;
+        } else {
+            chooseComponent = <Auth/>;
+        }
         return (
             <div className="nav z-depth-2">
                 <div className="logo-center left col s12 valign-wrapper">
-                    <a href="#" className="brand-logo"><img src="img/landingPage/logo.png" className="valign" alt="logo"/></a>
+                    <a href="#" className="brand-logo"><img src="img/landingPage/logo.png" className="valign"
+                                                            alt="logo"/></a>
                 </div>
                 <Search/>
-                <Auth/>
+                {chooseComponent}
             </div>
         )
     }
---
+++ views/components/index/header/userinfo.jsx
@@ -0,0 +1,27 @@
+var React = require("react");
+
+var UserInfo = React.createClass({
+    render: function () {
+        return (
+            <div className="user right hide-on-med-and-down">
+                <img src="../img/Layer-12.png" alt="profile" className="circle responsive-img valign profile-image"/>
+
+                <a className="btn-flat dropdown-button waves-effect waves-light white-text center-align" href="#"
+                   data-activates="dropdown1">Èìÿ<i className="mdi-navigation-arrow-drop-down right"></i></a>
+
+                <ul id="dropdown1" className="dropdown-content">
+
+                    <li><a href="#"><i className="mdi-action-face-unlock"></i> Profile</a></li>
+                    <li><a href="#"><i className="mdi-action-settings"></i> Settings</a></li>
+                    <li><a href="#"><i className="mdi-communication-live-help"></i> Help</a></li>
+                    <li className="divider"></li>
+                    <li><a href="#"><i className="mdi-action-lock-outline"></i> Lock</a></li>
+                    <li><a href="#"><i className="mdi-hardware-keyboard-tab"></i> Logout</a></li>
+
+                </ul>
+            </div>
+
+        )
+    }
+});
+module.exports = UserInfo;
