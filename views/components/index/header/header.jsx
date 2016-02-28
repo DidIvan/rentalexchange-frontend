84fb381f5ca3d3b203334889c6f903ec9e39ed33
Ivan <Holy.vinil@gmail.com>
2016-02-28 20:36:27 +0200

    some updates

--- views/components/index/header/header.jsx
+++ views/components/index/header/header.jsx
@@ -4,14 +4,44 @@ var React = require("react"),
     authenticator = require('app/authenticator'),
     UserInfo = require('./userinfo.jsx');
 var Header = React.createClass({
+    getInitialState: function () {
+        return {
+            isLogined: authenticator.isAuthenticate
+        }
+    },
+    getLogOut: function (event) {
+        this.setState({isLogined: false});
+  /*      authenticator.errorLoginFunction();*/
+    },
     render: function () {
+        console.log("3 "+ authenticator.isAuthenticate.valueOf());
         var chooseComponent;
-        if (authenticator.login) {
-            console.log("true");
-            chooseComponent = <UserInfo/>;
+        if (this.state.isLogined) {
+            console.log("1 "+ this.state.isLogined.valueOf());
+            chooseComponent =
+                <div className="user right hide-on-med-and-down">
+                    <img src="img/landingPage/Layer-12.png" alt="profile"
+                         className="circle responsive-img valign profile-image"/>
+
+                    <a className="btn-flat dropdown-button waves-effect waves-light white-text center-align" href="#"
+                       data-activates="dropdown1">Name<i className="mdi-navigation-arrow-drop-down left"></i></a>
+
+                    <ul id="dropdown1" className="dropdown-content">
+
+                        <li><a href="#"><i className="mdi-action-face-unlock"></i> Profile</a></li>
+                        <li><a href="#"><i className="mdi-action-settings"></i> Settings</a></li>
+                        <li><a href="#"><i className="mdi-communication-live-help"></i> Help</a></li>
+                        <li className="divider"></li>
+                        <li><a href="#"><i className="mdi-action-lock-outline"></i> Lock</a></li>
+                        <li><a href="#"><i className="mdi-hardware-keyboard-tab" onClick={this.getLogOut}></i>
+                            Logout</a>
+                        </li>
+
+                    </ul>
+                </div>;
         } else {
             chooseComponent = <Auth/>;
-            console.log("false");
+            console.log("2 "+this.state.isLogined.valueOf());
         }
         return (
             <div className="nav z-depth-2">
--- views/components/index/header/userinfo.jsx
+++ views/components/index/header/userinfo.jsx
@@ -1,13 +1,18 @@
-var React = require("react");
+var React = require("react"),
+    authenticator = require('app/authenticator');
 
 var UserInfo = React.createClass({
+    getLogOut: function(){
+        authenticator.errorLoginFunction();
+    },
     render: function () {
         return (
             <div className="user right hide-on-med-and-down">
-                <img src="img/landingPage/Layer-12.png" alt="profile" className="circle responsive-img valign profile-image"/>
+                <img src="img/landingPage/Layer-12.png" alt="profile"
+                     className="circle responsive-img valign profile-image"/>
 
                 <a className="btn-flat dropdown-button waves-effect waves-light white-text center-align" href="#"
-                   data-activates="dropdown1">Name<i className="mdi-navigation-arrow-drop-down right"></i></a>
+                   data-activates="dropdown1">Name<i className="mdi-navigation-arrow-drop-down left"></i></a>
 
                 <ul id="dropdown1" className="dropdown-content">
 
@@ -16,7 +21,7 @@ var UserInfo = React.createClass({
                     <li><a href="#"><i className="mdi-communication-live-help"></i> Help</a></li>
                     <li className="divider"></li>
                     <li><a href="#"><i className="mdi-action-lock-outline"></i> Lock</a></li>
-                    <li><a href="#"><i className="mdi-hardware-keyboard-tab"></i> Logout</a></li>
+                    <li><a href="#"><i className="mdi-hardware-keyboard-tab" onClick={this.getLogOut}></i> Logout</a></li>
 
                 </ul>
             </div>
