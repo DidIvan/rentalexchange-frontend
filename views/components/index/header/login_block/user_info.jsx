653f82142fc5c8d9a63800e418da9042442dd229
Ivan <Holy.vinil@gmail.com>
2016-02-29 23:05:47 +0200

    some updates

--- views/components/index/header/login_block/user_info.jsx
+++ views/components/index/header/login_block/user_info.jsx
@@ -4,23 +4,26 @@
 var React = require("react");
 var UserInfo = React.createClass({
 
-/*    handleLogout: this.props.handleLogout(),*/
+    /*    handleLogout: this.props.handleLogout(),*/
 
     render: function () {
         return (
             <div className="user right hide-on-med-and-down">
-                <img src="img/store/avatars/test-ava.png" alt="profile"
+                <img src="img/landingPage/Layer-12.png" alt="profile"
                      className="circle responsive-img valign profile-image"/>
+
                 <a className="btn-flat dropdown-button waves-effect waves-light white-text center-align" href="#"
                    data-activates="dropdown1">Name<i className="mdi-navigation-arrow-drop-down left"></i></a>
+
                 <ul id="dropdown1" className="dropdown-content">
+
                     <li><a href="#"><i className="mdi-action-face-unlock"></i>Профайл</a></li>
                     <li><a href="#"><i className="mdi-action-settings"></i>Настройки</a></li>
                     <li><a href="#"><i className="mdi-communication-live-help"></i>Помощь</a></li>
                     <li className="divider"></li>
-                    <li><a href="#"><i className="mdi-hardware-keyboard-tab" onClick={this.props.handleLogout}></i>
-                        Выйти</a>
+                    <li><a href="#"><i className="mdi-hardware-keyboard-tab" onClick={this.props.handleLogout}></i>Выйти</a>
                     </li>
+
                 </ul>
             </div>
         );
